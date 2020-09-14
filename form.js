a25dc8f81490eafbaad39863ebc50f6d9c788fbf
Leandro Cerqueira Santos <ll_cerqueira@hotmail.com>
2020-09-14 18:04:36 -0300

    ultimas modificações

--- form.js
+++
@@ -1,9 +0,0 @@
-const email = document.getElementById('email');
-
-email.addEventListener('input', function (event) {
-	if (email.validity.typeMismatch) {
-		email.setCustomValidity('Email inválido.');
-	} else {
-		email.setCustomValidity('');
-	}
-});
